84410620c328e90290b864ac71946a24b2e49d34
BetterTomorrowPy <13540471106@163.com>
2017-05-11 10:59:24 +0800

    componts header and demo.

---
+++ src/components/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header id="head_top">
+    <slot name="logo"></slot>
+    <slot name="search"></slot>
+    <section class="head_goback" v-if="goBack" @click="$router.go(-1)">
+      <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" version="1.2">
+        <polyline></polyline>
+      </svg>
+    </section>
+  </header>
+</template>
