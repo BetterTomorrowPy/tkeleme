14a0619adca961071d9111d2546f1efc541fc27b
BetterTomorrowPy <13540471106@163.com>
2017-05-22 13:44:22 +0800

    update demo

---
+++ src/components/democompont.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="show = !show">
+      点我
+    </button>
+    <transition name="fade">
+      <p v-if="show">show me.</p>
+    </transition>
+  </div>
+</template>
+
+<style>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-active {
+    opacity: 0;
+  }
+</style>
+
+<script>
+  export default{
+    data(){
+      return {
+        show: true
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Hello from '../components/Hello.vue'
+import TransitionDemo from '../components/democompont.vue'
 
 var routes = [
   {
     path: '/hello',
     name: 'Hello',
     component: Hello
+  },
+  {
+    path: '/demo',
+    name: 'transition',
+    component: TransitionDemo
   }
 ]
 
